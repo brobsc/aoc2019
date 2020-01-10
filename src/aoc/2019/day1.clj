1d1412af9e405d4ff9b9a507735e6ab30d1e4ca9
Rob <bruno.robsiq@gmail.com>
2020-01-10 15:15:31 -0300

    Refactor day1

--- src/aoc/2019/day1.clj
+++ src/aoc/2019/day1.clj
@@ -1,32 +1,39 @@
 (ns aoc.2019.day1
-  (:require [aoc.core :refer [read-input]]))
+  (:require [aoc.core :refer [read-input]]
+            [clojure.test :refer [testing is]]))
+(def input
+  (->> (read-input 2019 1)
+       (map #(Integer/parseInt %))))
+
+(defn fuel [i]
+  (let [result (-> i (/ 3.0) (- 2) (biginteger))]
+    (if (pos? result) result 0)))
 
 (defn calc-fuel [i]
-  (let [sum (-> i
-                (/ 3.0)
-                (Math/floor)
-                (- 2)
-                (biginteger))]
-    (if (< sum 0) 0 sum)))
+  (->> i
+       (iterate fuel)
+       (next)
+       (take-while pos?)
+       (apply +)))
 
 (defn part-1 []
-  (->> (read-input 2019 1)
-       (map #(Integer/parseInt %))
+  (->> input
+       (map fuel)
+       (apply +)))
+
+(defn part-2 []
+  (->> input
        (map calc-fuel)
        (apply +)))
 
-(defn calc-fuel' [i]
-  (loop [fuel i
-         t 0]
-    (let [extra (calc-fuel fuel)
-          total (+ t extra)]
-      (if (< 0 extra)
-        (recur extra total)
-        total))))
+(testing "Part 1"
+  (is (= (fuel 14) 2))
+  (is (= (fuel 1969) 654))
+  (is (= (fuel 100756) 33583))
+  (is (= (part-1) 3320816)))
 
-(defn part-2 []
-  (->> (read-input 2019 1)
-       (map #(Integer/parseInt %))
-       (map calc-fuel')
-       (apply +)))
-
+(testing "Part 2"
+  (is (= (calc-fuel 14) 2))
+  (is (= (calc-fuel 1969) 966))
+  (is (= (calc-fuel 100756) 50346))
+  (is (= (part-2) 4978360)))
